a05045f82fad9215dd9cbe291efb0ba2fe7d7d8a
Moritz Beste <159854683+moritzbeste@users.noreply.github.com>
2026-05-01 19:36:15 +0200

    work in progress Assignment 1

--- Exercise1/template/assignment_1.1.py
+++ Exercise1/template/assignment_1.1.py
@@ -5,15 +5,24 @@ import numpy.typing as npt
 def load_grades(filename: str) -> npt.NDArray:
     # TODO: read grades from the file.
     # ====================================================================
-    grades = np.array([], dtype=float)
+    with open(filename, 'r') as file:
+        grades = [float(line) for line in file]
     # ====================================================================
-    return grades
+    return np.array(grades)
 
 
 def python_compute(array: npt.NDArray) -> tuple[float, float]:
     # TODO: compute the mean and the variance using standard Python.
     # ====================================================================
-    mean, var = 0, 1
+    mean = 0
+    for grade in array:
+        mean += grade
+    mean /= len(array)
+
+    var = 0
+    for grade in array:
+        var += (grade - mean)**2
+    var /= (len(array) - 1)
     # ====================================================================
     return mean, var
 
@@ -21,7 +30,8 @@ def python_compute(array: npt.NDArray) -> tuple[float, float]:
 def numpy_compute(array: npt.NDArray, ddof: int = 0) -> tuple[float, float]:
     # TODO: compute the mean and the variance using numpy.
     # ====================================================================
-    mean, var = 0, 1
+    mean = np.mean(array)
+    var = np.var(array, ddof=1)
     # ====================================================================
     return mean, var
 
@@ -30,5 +40,9 @@ if __name__ == "__main__":
     # TODO: load the grades from the file, compute the mean and the
     # variance using both implementations and report the results.
     # ====================================================================
-    pass
+    arr = load_grades(filename="./data/G.txt")
+    mean1, var1 = python_compute(arr)
+    mean2, var2 = numpy_compute(arr)
+    print(mean1, var1)
+    print(mean2, var2)
     # ====================================================================
--- Exercise1/template/assignment_1.2.py
+++ Exercise1/template/assignment_1.2.py
@@ -11,7 +11,7 @@ def sample_normal(
 ) -> npt.NDArray:
     # TODO: generate samples from multivariate normal distribution.
     # ====================================================================
-    samples = np.zeros((len(mu_target), n_samples))
+    samples = np.random.multivariate_normal(mu_target, V_target, size=n_samples).T
     # ====================================================================
     return samples
 
@@ -19,21 +19,35 @@ def sample_normal(
 def compute_moments(samples: npt.NDArray) -> tuple[npt.NDArray, npt.NDArray]:
     # TODO: estimate mean and covariance of the samples.
     # ====================================================================
+    # mean = np.mean(samples, axis=1)
+    # covariance = np.cov(samples, ddof=1)
+
     mean = np.zeros(samples.shape[0])
+    for i in range(samples.shape[1]):
+        mean += np.array([samples[0][i], samples[1][i]])
+    mean /= 2
     covariance = np.zeros((samples.shape[0], samples.shape[0]))
+
     # ====================================================================
-    return mean, covariance
+    return mean, covariances
 
 
 if __name__ == "__main__":
     # TODO: define the parameters of the simulation.
     # ====================================================================
-    pass
+    mu = np.array([-0.4, 1.1])
+    V = np.array([[2, 0.4], [0.4, 1]])
+    sample_sizes = [10, 100, 1000, 10000]
     # ====================================================================
 
     # TODO: estimate mean, covariance, and compute the required errors.
     # ====================================================================
-    pass
+    vals = []
+    for s in sample_sizes:
+        sample = sample_normal(n_samples=s, mu_target=mu, V_target=V)
+        mean, var = compute_moments(sample)
+
+
     # ====================================================================
 
     # TODO: plot the results on the log-log scale.
