b1639bfa502311b678df75e037b2fda9c7980a9f
moritzbeste <bestemoritz3@gmail.com>
2026-07-04 17:32:49 +0200

    wip exercise3 assignment1

--- Exercise3/assignment_1.py
+++ Exercise3/assignment_1.py
@@ -16,7 +16,17 @@ def get_distribution(
     """Creates the joint distribution over the stochastic parameters."""
 
     # TODO: create a joint distribution over the stochastic parameters.
-    return cp.Uniform()
+    c_uniform = cp.Uniform(c_lims[0], c_lims[1])
+    k_uniform = cp.Uniform(k_lims[0], k_lims[1])
+    f_uniform = cp.Uniform(f_lims[0], f_lims[1])
+    y0_uniform = cp.Uniform(y0_lims[0], y0_lims[1])
+    y1_uniform = cp.Uniform(y1_lims[0], y1_lims[1])
+
+    dists = [c_uniform, k_uniform, f_uniform, y0_uniform, y1_uniform]
+
+    joint_uniform = cp.J(*dists)
+    
+    return joint_uniform
 
 
 def run_method(method, **kwargs):
@@ -31,32 +41,53 @@ def run_method(method, **kwargs):
 
 if __name__ == "__main__":
     # TODO: set the stochastic parameters.
-    c_lims = [None, None]
-    k_lims = [None, None]
-    f_lims = [None, None]
-    y0_lims = [None, None]
-    y1_lims = [None, None]
+    c_lims = [8e-2, 12e-2]
+    k_lims = [3e-2, 4e-2]
+    f_lims = [8e-2, 12e-2]
+    y0_lims = [45e-2, 55e-2]
+    y1_lims = [-5e-2, 5e-2]
 
     # TODO: set the determinisic parameters.
-    fixed_args = {"omega": None}
+    fixed_args = {"omega": 1.0}
 
     # TODO: set the parameters of the methods.
-    quadrature_degree = None
-    pce_degree = None
+    quadrature_degree = [3, 4] # K
+    pce_degree = [3, 4] # N
     n_samples = None
 
     # TODO: set the time domain
-    T_max = None
-    dt = None
-    t_grid = np.arange(0, T_max + dt, dt)
+    T_min = 0
+    T_max = 10
+    dt = 0.01
+    t_grid = np.arange(T_min, T_max + dt, dt)
 
     ###########################################################################
 
     # TODO: define the distribution over the stochastic parameters.
+    joint_uniform = get_distribution(c_lims=c_lims, k_lims=k_lims, f_lims=f_lims, y0_lims=y0_lims, y1_lims=y1_lims)
 
     # TODO: run the pseudo-spectral method on full grid.
-
+    for quad_deg in quadrature_degree:
+        for pce_deg in pce_degree:
+            pseudo_spectral_sobol(
+                pce_degree=pce_deg, 
+                quadrature_degree=quad_deg, 
+                distribution=joint_uniform, 
+                t_grid=t_grid, 
+                fixed_args=fixed_args, 
+                sparse=False,
+            )
     # TODO: run the pseudo-spectral method on sparse grid.
+    for quad_deg in quadrature_degree:
+        for pce_deg in pce_degree:
+            pseudo_spectral_sobol(
+                pce_degree=pce_deg, 
+                quadrature_degree=quad_deg, 
+                distribution=joint_uniform, 
+                t_grid=t_grid, 
+                fixed_args=fixed_args, 
+                sparse=True,
+            )
 
     # TODO: run the Monte Carlo method.
 
--- Exercise3/utils/sobol.py
+++ Exercise3/utils/sobol.py
@@ -26,6 +26,27 @@ def monte_carlo_sobol(
     return 0, 0
 
 
+def simulate_all(t_grid, nodes, fixed_args, method='odeint'):
+    # dists = [c_uniform, k_uniform, f_uniform, y0_uniform, y1_uniform]
+    solutions = np.zeros(nodes.shape[1])
+    for i in range(nodes.shape[1]):
+        c  = nodes[0, i]
+        k  = nodes[1, i]
+        f  = nodes[2, i]
+        y0 = nodes[3, i]
+        y1 = nodes[4, i]
+
+        omega = fixed_args['omega']
+
+        osc = Oscillator(c, k, f, omega)
+
+        ys = osc.discretize(method=method, y0=y0, y1=y1, t_grid=t_grid)
+        y_final = ys[-1]
+        solutions[i] = y_final
+    
+    return solutions
+
+
 def pseudo_spectral_sobol(
     pce_degree: int,
     quadrature_degree: int,
@@ -37,4 +58,18 @@ def pseudo_spectral_sobol(
     """Computes the Sobol' indices using a pseudo-spectral method."""
     
     # TODO: implement the pseduo-spectral method.
-    return 0, 0
+    # genreate poly basis
+    expansion = cp.generate_expansion(pce_degree, distribution)
+
+    # quad
+    nodes, weights = cp.generate_quadrature(quadrature_degree, distribution)
+
+    # eval
+    ys_final = simulate_all(t_grid, nodes, fixed_args)
+
+    pce = cp.fit_quadrature(expansion, nodes, weights, ys_final)
+
+    first = cp.Sens_m(pce, distribution)
+    total = cp.Sens_t(pce, distribution)
+
+    return first, total
